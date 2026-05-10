0feb34966ac8ab6f55d7a5ba4c06953382b776b7
unknown <yograjrajput027@gmail.com>
2026-05-10 14:46:09 +0530

    first commit

---
+++ src/main/java/com/dockerjenkinsintegration/jenkinsdockerintegration/JenkinsDockerIntegrationApplication.java
@@ -0,0 +1,27 @@
+package com.dockerjenkinsintegration.jenkinsdockerintegration;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerIntegrationApplication {
+
+    public static Logger logger = LoggerFactory.getLogger(JenkinsDockerIntegrationApplication.class);
+
+    @PostConstruct
+    public void test()
+    {
+        logger.info("Application is goint to start...");
+    }
+
+    public static void main(String[] args) {
+
+        logger.info("Application started...");
+
+        SpringApplication.run(JenkinsDockerIntegrationApplication.class, args);
+    }
+
+}
